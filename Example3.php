c1ac1fda1ed3f2cfa8de265dda9113de91d71ff4
NadaAlsalami <nadagaber324@yahoo.com>
2015-11-28 11:02:59 +0300

    Addition

--- Example3.php
+++ Example3.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,12 +6,16 @@
         <title></title>
     </head>
     <body>
-        <?php
+       <?php
 $t=date("H");
 if ($t<"20")
-  {
-  echo "Have a good day!";
-  }
-?> 
+   {
+   echo "Have a good day!";
+   }
+else
+   {
+   echo "Have a good night!";
+   }
+?>
     </body>
 </html>
